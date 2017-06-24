73b82ff020826c7b8a0d2e6fb8d5ef4c4bd2b447
wisamasd <wisam@walkmail.net>
2017-06-24 19:30:03 +0300

    Create DuoKeySwitcher.meta.js

---
+++ DuoKeySwitcher.meta.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name         DuoKeySwitcher
+// @namespace    duolingo
+// @version      0.3.0
+// @description  The script provide you ability to type right symbols in answer box without switching keyboard layout. Based on the works of Lifeshade (aka HeadwayCourse) and ilnicki
+// @author       wisamasd 
+// @include      https://www.duolingo.com/*
+// @require      http://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js
+// @updateURL		 none
+// @downloadURL	 none
+// @grant        none
+// ==/UserScript==
